cbe01f6ec30118aafde0e04d2502b062868dc018
Hiirsh <47283490+Hiirsh@users.noreply.github.com>
2022-05-07 19:58:41 +0300

    homework2_2022_05_07

---
+++ hw2_2022_05_03/src/Homework2.java
@@ -0,0 +1,16 @@
+package src;
+public class Homework2{
+    public static void main(String[] args) {
+        thunder1();
+        thunder2();
+    }
+private static void thunder1() {
+    System.out.println("Люблю грозу в начале мая,");
+    System.out.println("Когда весенний, первый гром,");
+    System.out.println("как бы резвяся и играя,");
+    System.out.println("Грохочет в небе голубом.");
+}
+private static void thunder2() {
+    System.out.printf("\nГремят раскаты молодые,\nВот дождик брызнул, пыль летит,\nПовисли перлы дождевые,\nИ солнце нити золотит.\n");
+}
+}
