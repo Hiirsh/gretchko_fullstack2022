97a6463d7654701af194614c479539f64ff11eed
Hiirsh <47283490+Hiirsh@users.noreply.github.com>
2022-05-16 17:34:47 +0300

    hw5

--- cw04_date2022_05_12/src/App.java
+++
@@ -1,9 +0,0 @@
-public class App {
-    public static void main(String[] args) {
-        System.out.println(checkArmy(18));
-    }
-
-    public static String checkArmy(int age) {
-        return age >= 18 && age < 45 ? "Go to army" : "Go to pub";
-    }
-}
